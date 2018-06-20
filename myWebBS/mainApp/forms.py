88c1f0aa5cba6b9ebbd05ab7dd9e634d50ebed90
RnGoL <RnGoL0105@gmail.com>
2018-06-20 22:18:44 +0300

    work at home 2

--- myWebBS/mainApp/forms.py
+++ myWebBS/mainApp/forms.py
@@ -1,7 +1,47 @@
 from django import forms
+from django.forms import SelectDateWidget
 
-class searchForm(forms.Form):
+from mainApp.models import Author
+
+
+# Формы не выводятся на страницу
+# Не большие вопросы по шаблонам Django и их расширению
+
+class SearchForm(forms.Form):
     search_field = forms.CharField()
+
+
+class AddAuthorForm(forms.Form):
+    name = forms.CharField(max_length=32)
+    surname = forms.CharField(max_length=64)
+    age = forms.IntegerField(max_value=130)
+
+
+# Так создавали форму на занятиях но у меня не выходит создать таким способом
+
+# from django import forms
+# from django.forms import SelectDateWidget
+# from ipdb import set_trace
+#
+# from blog.models import Post
+#
+#
+# class CreatePostForm(forms.ModelForm):
+#     class Meta:
+#         model = Post
+#         fields = ('title', 'text', 'published_date')
+#
+#     def __init__(self, user, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.user = user
+#         self.fields['published_date'].widget = SelectDateWidget()
+#
+#
+#     def save(self, commit=True):
+#         post = self.instance
+#         post.author = self.user
+#         super().save()
+
 
 
 
